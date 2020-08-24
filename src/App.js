e0ad0ca0835299f7b45f5e620c6c3113807e97af
Ximena Fuertes <ximenafuertes@gmail.com>
2020-08-24 20:56:20 -0300

    update-isOnline-status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import {
   NotificationContainer,
-  NotificationManager,
-} from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
-import OnlineStatusMock from './OnlineStatusMock';
-import './App.css';
+  NotificationManager
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
+import OnlineStatusMock from "./OnlineStatusMock";
+import "./App.css";
 
 /* 
 Feel free to edit this all. If you don't need the HoC, go remove it. 
@@ -13,7 +13,7 @@ If you wish to save the state somewhere else, go for it.
 Just keep rendering <OnlineStatusMock /> 
 */
 
-const withOnlineStatus = WrappedComponent =>
+const withOnlineStatus = (WrappedComponent) =>
   class WithOnlineStatus extends React.Component {
     constructor(props) {
       super(props);
@@ -24,7 +24,7 @@ const withOnlineStatus = WrappedComponent =>
       return (
         <>
           <OnlineStatusMock
-            onIsOnlineChange={isOnline => this.setState({ isOnline })}
+            onIsOnlineChange={(isOnline) => this.setState({ isOnline })}
           />
           <WrappedComponent {...this.props} isOnline={isOnline} />
         </>
@@ -33,16 +33,51 @@ const withOnlineStatus = WrappedComponent =>
   };
 
 class App extends React.Component {
-  componentDidUpdate({ isOnline }) {
-    NotificationManager.info(isOnline ? 'Online' : 'Offline');
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: [],
+      isLessThanSec: false
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.isOnline !== this.props.isOnline) {
+      this.setState((prevState) => ({
+        buffer: [...prevState.buffer, this.props.isOnline]
+      }));
+
+      if (prevProps.isOnline && !this.props.isOnline) {
+        new Promise((resolve) => {
+          this.setState({ isLessThanSec: true });
+          setTimeout(() => {
+            this.setState({ isLessThanSec: false });
+            resolve();
+          }, 2000);
+        }).then(() => {
+          while (this.state.buffer.length) {
+            NotificationManager.info(
+              this.state.buffer[0] ? "Online" : "Offline"
+            );
+            this.state.buffer.shift();
+          }
+        });
+      } else {
+        if (!this.state.isLessThanSec) {
+          NotificationManager.info(this.props.isOnline ? "Online" : "Offline");
+          this.state.buffer.shift();
+          this.state.buffer.shift();
+        }
+      }
+    }
   }
 
   render() {
     const { isOnline } = this.props;
     return (
       <>
-        <div className={isOnline ? 'online' : 'offline'}>
-          {isOnline ? 'Online' : 'Offline'}
+        <div className={isOnline ? "online" : "offline"}>
+          {isOnline ? "Online" : "Offline"}
           <NotificationContainer />
         </div>
       </>
